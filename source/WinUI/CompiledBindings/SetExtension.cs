52d359eace86d1c1472b99026e030f7a9d9ede78
Vitali Lesheniuk <levital@hotmail.de>
2022-12-09 09:55:07 +0100

    Added WinUI SetExtension

---
+++ source/WinUI/CompiledBindings/SetExtension.cs
@@ -0,0 +1,16 @@
+﻿#nullable enable
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Markup;
+
+namespace CompiledBindings.Markup;
+
+public class SetExtension : MarkupExtension
+{
+	public string? Path { get; set; }
+
+	protected override object? ProvideValue(IXamlServiceProvider serviceProvider)
+	{
+		return null;
+	}
+}
