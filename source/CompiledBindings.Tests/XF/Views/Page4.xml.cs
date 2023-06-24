d7d72a0457c06912c036a3cedbd47652a71c0f83
Vitali Lesheniuk <levital@hotmail.de>
2023-06-24 11:38:00 +0200

    Added new cases to tests

--- source/CompiledBindings.Tests/XF/Views/Page4.xml.cs
+++ source/CompiledBindings.Tests/XF/Views/Page4.xml.cs
@@ -1,4 +1,6 @@
-﻿namespace XFTest.Views;
+﻿#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+namespace XFTest.Views;
 
 partial class Page3
 {
@@ -10,6 +12,9 @@ class Page4_picker2_ItemDisplayBinding
 
 class Page4_DataTemplate0
 {
+	public global::Xamarin.Forms.IValueConverter inverseBooleanConverter { get; set; }
+	public global::Xamarin.Forms.Color Color1 { get; set; }
+	public global::Xamarin.Forms.Color Color2 { get; set; }
 }
 
 class Page4_DataTemplate0_picker1_ItemDisplayBinding
